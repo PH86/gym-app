7b076d162c3ffce7d0f89d5a6240569742125f32
PH86 <49610592+PH86@users.noreply.github.com>
2020-11-23 14:35:22 +0000

    final second commit button links

--- src/components/Button.js
+++ src/components/Button.js
@@ -17,7 +17,7 @@ const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle : STYLES[0]
 const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0]
 
 return (
-    <Link to="/sign-up" className="btn-mobile">
+    <Link to="/gym-app/sign-up" className="btn-mobile">
         <button className={`btn ${checkButtonStyle} ${checkButtonSize}`}
         onClick={onClick}
         type={type}
